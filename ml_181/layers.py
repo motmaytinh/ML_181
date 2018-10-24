9a7d1c4790a72fbc2ba46ac2ebc089694ca2818b
Nam Vu Quang <vqnam97@gmail.com>
2018-10-24 14:38:45 +0700

    max pool native forward

--- ml_181/layers.py
+++ ml_181/layers.py
@@ -12,14 +12,39 @@ def conv_naive_forward(x, w, b, conv_param):
     Hout = int((W - WW + 2 * pad) / stride + 1)
     Wout = int((H - HH + 2 * pad) / stride + 1)
     out = np.zeros((N, F, Hout, Wout))
-    for i in range(N):
-        image = x_padded[i, :, :, :]
-        for j in range(Hout):
-            for k in range(Wout):
-                im_patch = image[:, j * stride:j *
-                                 stride + HH, k * stride:k * stride + WW]
+    for idx_image, each_image in enumerate(x_padded):
+        for i_H in range(Hout):
+            for i_W in range(Wout):
+                im_patch = each_image[:, i_H * stride:i_H * stride + HH,
+                                      i_W * stride:i_W * stride + WW]
                 scores = (w * im_patch).sum(axis=(1, 2, 3)) + b
-                out[i, :, j, k] = scores
+
+                out[idx_image, :, i_H, i_W] = scores
 
     cache = (x, w, b, conv_param)
+    return out, cache
+
+
+def max_pool_naive_forward(x, pool_param):
+    (N, C, H, W) = x.shape
+
+    pool_height, pool_width, stride = pool_param['pool_height'], pool_param['pool_width'], pool_param['stride']
+
+    Hout = int(1 + (H - pool_height) / stride)
+    Wout = int(1 + (W - pool_width) / stride)
+
+    out = np.zeros((N, C, Hout, Wout))
+
+    for idx_image, each_image in enumerate(x):
+        for i_H in range(Hout):
+            for i_W in range(Wout):
+
+                each_window_channels = each_image[:, i_H*stride: i_H*stride + pool_height,
+                                                i_W*stride: i_W*stride + pool_width ]
+
+                out[idx_image, :, i_H, i_W] = each_window_channels.max(axis = (1,2)) # maxpooling
+
+
+    cache = (x, pool_param)
+
     return out, cache
